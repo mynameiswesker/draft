2cbff946513b270f78d76a20b1b8203eb500cf7f
mynameiswesker <alexandr_071@bk.ru>
2020-06-11 13:39:36 +0400

    Updates

---
+++ precache-manifest.967ef8059ce4063bb4d9ec211e1c7188.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a8ab969a38f5b1200116cc451bd53b",
+    "url": "/draft/index.html"
+  },
+  {
+    "revision": "f44a22797023672aee47",
+    "url": "/draft/static/css/main.5c216878.chunk.css"
+  },
+  {
+    "revision": "16c8762aae1cae8ec67f",
+    "url": "/draft/static/js/2.8f116fdc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/draft/static/js/2.8f116fdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44a22797023672aee47",
+    "url": "/draft/static/js/main.f01dd35b.chunk.js"
+  },
+  {
+    "revision": "50bc4f041eba28e83f95",
+    "url": "/draft/static/js/runtime-main.e0c2b45b.js"
+  },
+  {
+    "revision": "dd261913dd3b47b60b54cb29ec00ef17",
+    "url": "/draft/static/media/AbbyssalBlade.dd261913.png"
+  },
+  {
+    "revision": "6a4946aa3e8b6586275fafa6c4249924",
+    "url": "/draft/static/media/AeonDisk.6a4946aa.png"
+  },
+  {
+    "revision": "4fa2e42b6e5df87c2a00d765f885250f",
+    "url": "/draft/static/media/AetherLens.4fa2e42b.png"
+  },
+  {
+    "revision": "e936e9548a1c95e563adeea271565cb1",
+    "url": "/draft/static/media/AssaultCuirass.e936e954.png"
+  },
+  {
+    "revision": "5958ebdf61ebc8e8a1a3ae357502aa78",
+    "url": "/draft/static/media/BattleFury.5958ebdf.png"
+  },
+  {
+    "revision": "3b8f9ebd1b5b5627941f5d8ec418a93e",
+    "url": "/draft/static/media/BlackKingBar.3b8f9ebd.png"
+  },
+  {
+    "revision": "506494b245eefa65b0f378ed1140eca4",
+    "url": "/draft/static/media/BladeMail.506494b2.png"
+  },
+  {
+    "revision": "5c587303a304264d593e3275d59a240e",
+    "url": "/draft/static/media/BlinkDagger.5c587303.png"
+  },
+  {
+    "revision": "904384c9ba99c4eda55f6eb48b145cca",
+    "url": "/draft/static/media/Butterfly.904384c9.png"
+  },
+  {
+    "revision": "745a8424d184244720df4b864817fb58",
+    "url": "/draft/static/media/CrimsonGuard.745a8424.png"
+  },
+  {
+    "revision": "4732971b62384aa8e1f90fc64a44a371",
+    "url": "/draft/static/media/Desolator.4732971b.png"
+  },
+  {
+    "revision": "00e05eeb569c83c3d04cd8f275ea068c",
+    "url": "/draft/static/media/DiffusalBlade.00e05eeb.png"
+  },
+  {
+    "revision": "4ce68a83735ecee86c1cee60742acff1",
+    "url": "/draft/static/media/DivineRapier.4ce68a83.png"
+  },
+  {
+    "revision": "e2e5de06cac6277838a930a863c88afb",
+    "url": "/draft/static/media/DrumofEndurance.e2e5de06.png"
+  },
+  {
+    "revision": "40f9a5b9e8e37c5406056a6631b12873",
+    "url": "/draft/static/media/EchoSabre.40f9a5b9.png"
+  },
+  {
+    "revision": "8b1d73e69316c8f5c8f75f05e7e67680",
+    "url": "/draft/static/media/EtherealBlade.8b1d73e6.png"
+  },
+  {
+    "revision": "6172ac2e1378643c5d2bf8aef330bd16",
+    "url": "/draft/static/media/EulsScepterofDivinity.6172ac2e.png"
+  },
+  {
+    "revision": "7631b706d8bb58e717ba5365f47a3df5",
+    "url": "/draft/static/media/EyeofSkadi.7631b706.png"
+  },
+  {
+    "revision": "6c5a66d5b0f99e1767c629622edc58e4",
+    "url": "/draft/static/media/GhostScepter.6c5a66d5.png"
+  },
+  {
+    "revision": "ff8c615b7de051e2a9cb87d46d9fd291",
+    "url": "/draft/static/media/GlimmerCape.ff8c615b.png"
+  },
+  {
+    "revision": "18963f3b1dc0829d58d5696f9f5f3b9b",
+    "url": "/draft/static/media/GuardianGreaves.18963f3b.png"
+  },
+  {
+    "revision": "7f711c85ee79c22cada2bc01f624a57f",
+    "url": "/draft/static/media/HeavensHalberd.7f711c85.png"
+  },
+  {
+    "revision": "8d8d258dcbb3cbc6523b11834ba73b8a",
+    "url": "/draft/static/media/HurricanePike.8d8d258d.png"
+  },
+  {
+    "revision": "c08742913768a11acca950cfcbffe8ea",
+    "url": "/draft/static/media/KayaandSange.c0874291.png"
+  },
+  {
+    "revision": "166c292b4b6a18c301a99f3cab1e453a",
+    "url": "/draft/static/media/LinkensSphere.166c292b.png"
+  },
+  {
+    "revision": "62f68d3c7c58aaadc93179fca38526e5",
+    "url": "/draft/static/media/LotusOrb.62f68d3c.png"
+  },
+  {
+    "revision": "beb2114bbd39eae0bcad80c52b625e5f",
+    "url": "/draft/static/media/MantaStyle.beb2114b.png"
+  },
+  {
+    "revision": "921e5f06752bf49513c214b21178ccd7",
+    "url": "/draft/static/media/Mekansm.921e5f06.png"
+  },
+  {
+    "revision": "5611368068a9bc65e34bef4d6fead5c5",
+    "url": "/draft/static/media/MeteorHammer.56113680.png"
+  },
+  {
+    "revision": "02e23390bf453d311d98dd26630b877d",
+    "url": "/draft/static/media/Mjollnir.02e23390.png"
+  },
+  {
+    "revision": "8025e2e4b4f899c7b535c04425bdaf64",
+    "url": "/draft/static/media/OrchidMalevolence.8025e2e4.png"
+  },
+  {
+    "revision": "42a504c13c63ad70d92f40475f6aaac1",
+    "url": "/draft/static/media/PipeofInsight.42a504c1.png"
+  },
+  {
+    "revision": "b596f4be457642ff02bbc0b466349c68",
+    "url": "/draft/static/media/Radiance.b596f4be.png"
+  },
+  {
+    "revision": "b6f69cf7e950e739fc16a9b08fd35d26",
+    "url": "/draft/static/media/SangeandYasha.b6f69cf7.png"
+  },
+  {
+    "revision": "061f9ad8d6a4d62f43e1ed4f87da057f",
+    "url": "/draft/static/media/Satanic.061f9ad8.png"
+  },
+  {
+    "revision": "5f5935d69888bbe3b09486a814d528e5",
+    "url": "/draft/static/media/ScytheofVyse.5f5935d6.png"
+  },
+  {
+    "revision": "c2a7bbc10d23844c6fceb098c76d415a",
+    "url": "/draft/static/media/ShivasGuard.c2a7bbc1.png"
+  },
+  {
+    "revision": "11bab6d46837bda976930f771c038ca9",
+    "url": "/draft/static/media/SilverEdge.11bab6d4.png"
+  },
+  {
+    "revision": "6aee02223ddad4b2684146e33f833ffc",
+    "url": "/draft/static/media/SkullBasher.6aee0222.png"
+  },
+  {
+    "revision": "ca520cb4d93f8645ef689906f06866e3",
+    "url": "/draft/static/media/SolarCrest.ca520cb4.png"
+  },
+  {
+    "revision": "cd70b0d066721707cbdae31deaaf387a",
+    "url": "/draft/static/media/SpiritVessel.cd70b0d0.png"
+  },
+  {
+    "revision": "9373e5401fc9f96322f55a22277d0b28",
+    "url": "/draft/static/media/UrnofShadows.9373e540.png"
+  },
+  {
+    "revision": "9cf353c8bb390d40c86dda393b2e40a1",
+    "url": "/draft/static/media/Vanguard.9cf353c8.png"
+  },
+  {
+    "revision": "6b9ee296c5c788842bc9b9663615596a",
+    "url": "/draft/static/media/VladmirsOffering.6b9ee296.png"
+  },
+  {
+    "revision": "c3e28833f72bb7baa4797df14cb5a734",
+    "url": "/draft/static/media/background_dota2.c3e28833.jpg"
+  }
+]);
